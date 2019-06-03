6c1a35ecd696e69553574f89c3bae5f991acf791
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-06-03 11:47:14 +0100

    While and For Loops

---
+++ forloop2.java
@@ -0,0 +1,14 @@
+class forloop2{
+	public static void main(String xyz[]){
+		int a,b,c;
+		a=1;
+		b=10;
+		c=100;
+	for (;a<=1&&b<=100&&c<=100;){
+		System.out.println(a+"-"+b+"-"+c);
+		a++;
+		b+=10;
+		c+=100;
+	}
+	}
+}
---
+++ whileloop.java
@@ -0,0 +1,10 @@
+class whileloop{
+	public static void main(String xyz[]){
+		int A;
+		A=1;
+		while(A<=10){
+			System.out.println(A);
+			A+=1;
+		}
+	}
+}
