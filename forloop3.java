cd6a68e7e2eb7e1f0e49105cf156a2c3e9fe2690
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-06-03 11:55:49 +0100

    While and For Loops PART 2

---
+++ forloop3.java
@@ -0,0 +1,11 @@
+class forloop3{
+	public static void main(String xyz[]){
+		int a,b,c;
+		for(a=1,b=10,c=100;a<=10;){
+			System.out.println(a+"-"+b+"-"+c);
+			a++;
+			b+=10;
+			c+=100;
+		}
+	}
+}
