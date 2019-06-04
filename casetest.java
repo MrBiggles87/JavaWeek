ba9e805e5e98dec5e6948368e537d424528be301
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-06-04 09:08:16 +0100

    Uses CASE and SWITCH in Java

---
+++ casetest.java
@@ -0,0 +1,12 @@
+class casetest{
+	public static void main(String xyz[]){
+		int a;
+		a=2;
+		switch(a){
+			case 1:System.out.println("London");break;
+			case 2:System.out.println("Manchester");break;
+			case 3:System.out.println("Swindon");break;
+			case 4:System.out.println("Liverpool");break;
+		}
+	}
+}
